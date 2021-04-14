8ee783151bf550f828d3474c901045e8ad238c74
Young legend <jeffdanquah16@gmail.com>
2021-04-14 14:43:46 +0000

    git commit -m CoffeeShop

---
+++ src/CoffeeShop.java
@@ -0,0 +1,30 @@
+
+public class CoffeeShop {
+    public void addOrder (){
+        System.out.println("Do not do this");
+    }
+    //adds the name of the item to the end of orders array.
+    public void fufillOrder(){}
+    //All orders have been fufilled.
+    public void listOrders (){}
+    // returns the list of orders taken, otherwise an empty array.
+    public void dueAmount () {}
+    //returns the total amount due for the orders taken.
+    public void cheapestItem (){}
+    // returns the name of the cheapest item on the menu.
+    public void drinksOnly (){}
+    // returns only the item names of drink type on the menu.
+    public void foodOnly (){}
+    // returns only the item names of food type on the menu.
+
+    String CoffeeShop = new String("CoffeeShop");{
+    }
+
+
+    String menu =new String("menu");
+    String orders =new String("orders");
+
+}
+
+
+
